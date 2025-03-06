78b3652526932cf6cf04b9da9ae1b3c3f7f32802
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-06 20:15:09 +0600

    Change 36

--- LeaveManagementSystem.Web/Controllers/HomeController.cs
+++ LeaveManagementSystem.Web/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace LeaveManagementSystem.Web.Controllers
         {
             return View();
         }
- 
+
         public IActionResult Privacy()
         {
             return View();
