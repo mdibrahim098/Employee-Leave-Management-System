623926e2c720270e8e88d77e39183bcd691b9472
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-06 23:01:43 +0600

    Change 43

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using LeaveManagementSystem.Web.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateOfBirth = new DateTime(1954, 12, 01)
+            };
+            return View(data);
+        }
+    }
+}
