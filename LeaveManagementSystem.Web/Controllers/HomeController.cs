1807a9c29ee9037e590a69969b967383e21c8732
mdibrahim098 <ibrahimkhulilullahratan098@gmail.com>
2025-03-13 00:09:33 +0600

    Change 88

--- LeaveManagementSystem.Web/Controllers/HomeController.cs
+++ LeaveManagementSystem.Web/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace LeaveManagementSystem.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+               
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
